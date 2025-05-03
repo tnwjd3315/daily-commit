d1e49b70ba119d37048cf933681c510b69f04903
Sujeong Kim <miysj3315@gmail.com>
2025-05-03 18:44:38 +0200

    range function

--- if_statement.py
+++ if_statement.py
@@ -40,3 +40,10 @@ for i in range(5):
 list(range(5,10))
 list(range(0,10,3))
 list(range(-10,-100,-30))
+
+a = ['Mary', 'had', 'a', 'little', 'lamb']
+for i in range(len(a)):
+    print(i, a[i])
+# instead, enumerate()
+
+sum(range(4)) #0+1+2+3
