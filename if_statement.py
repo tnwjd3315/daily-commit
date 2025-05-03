a57a3674328f62155bc1df40397408d791a65c21
Sujeong Kim <miysj3315@gmail.com>
2025-05-03 18:34:10 +0200

    for statement

--- if_statement.py
+++ if_statement.py
@@ -13,3 +13,22 @@ else:
   print("More")
 
 # for statement
+# measure some strings
+words = ['cat', 'window', 'defenestrate']
+for word in words:
+  print(word, len(word))
+
+# sample collection
+users = {'Hans':'active', 'Elena':'inactive','Sujeong':'active'}
+
+# difficult to edit the collection -> either copy or create new collection
+# stategy1: iterate over a copy
+for user, status in users.copy().items():
+  if status == 'inactive':
+    del users[user]
+
+# strategy2: create a new collection
+active_users={}
+for user, status in users.items():
+  if status == 'active':
+    active_users[user] = status
