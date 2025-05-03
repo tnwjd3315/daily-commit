d36d282b4f079cbd306ca3ef1ac66819c336bab3
Sujeong Kim <miysj3315@gmail.com>
2025-05-03 19:09:07 +0200

    break, continue pass, match statements

--- if_statement.py
+++ if_statement.py
@@ -47,3 +47,125 @@ for i in range(len(a)):
 # instead, enumerate()
 
 sum(range(4)) #0+1+2+3
+
+# break and continue statement
+for n in range(2,10):
+  for x in range(2,n):
+    if n % x==0:
+      print(f"{n} equals {x} * {n//x}")
+      break
+
+for n in range(2,10):
+  if n % 2 == 0:
+    print(f"Found an even number {n}")
+    continue
+  print(f"Found an odd number {n}")
+
+# break + else
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        # loop fell through without finding a factor
+        print(n, 'is a prime number')
+
+# pass statement - functions nothing
+while True:
+   pass
+
+# used for minimum functioned class
+class MyEmptyClass:
+   pass
+
+def initlog(*args):
+   pass
+
+# match statement
+def http_error(status):
+   match status:
+      case 400:
+         return "Bad requiest"
+      case 404:
+         return "Not found"
+      case 418:
+         return "I'm a teapot"
+      
+      case 401 | 403 | 404:
+         return "Not allowed"
+      
+      case _: # wildcard and never fails to match
+         return "Something's wrong with the internet"
+      
+# point is an (x, y) tuple
+def patterns(point):
+  match point:
+      case (0, 0):
+          print("Origin")
+      case (0, y):
+          print(f"Y={y}")
+      case (x, 0):
+          print(f"X={x}")
+      case (x, y):
+          print(f"X={x}, Y={y}")
+      case _:
+          raise ValueError("Not a point")
+     
+# use classes to structure the data
+class Point:
+   def __init__(self, x, y):
+      self.x = x
+      self.y = y
+
+def where_is(point):
+   match point:
+      case Point(x=0, y=0):
+          print("Origin")
+      case Point(x=0, y=y):
+          print(f"Y={y}")
+      case Point(x=x, y=0):
+          print(f"X={x}")
+      case Point():
+          print("Somewhere else")
+      case _:
+          print("Not a point")
+      
+Point(1, 2)
+Point(1, y=2)
+Point(x=1, y=2)
+Point(y=2, x=1)
+
+class Point:
+    __match_args__ = ('x', 'y')
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+match points:
+    case []:
+        print("No points")
+    case [Point(0, 0)]:
+        print("The origin")
+    case [Point(x, y)]:
+        print(f"Single point {x}, {y}")
+    case [Point(0, y1), Point(0, y2)]:
+        print(f"Two on the Y axis at {y1}, {y2}")
+    case _:
+        print("Something else")
+
+from enum import Enum
+class Color(Enum):
+    RED = 'red'
+    GREEN = 'green'
+    BLUE = 'blue'
+
+color = Color(input("Enter your choice of 'red', 'blue' or 'green': "))
+
+match color:
+    case Color.RED:
+        print("I see red!")
+    case Color.GREEN:
+        print("Grass is green")
+    case Color.BLUE:
+        print("I'm feeling the blues :(")
